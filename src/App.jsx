6505442e14fa09e73506727946d33319922cc858
jbrtherapy-cpu <jbrtherapy@gmail.com>
2025-10-15 12:42:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import GuidanceLibrary from './pages/GuidanceLibrary'
-import IslamicGuidance from './pages/IslamicGuidance'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Library from "./pages/Library";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export default function App(){
+function App() {
   return (
-    <div className="min-h-screen flex flex-col text-gray-900">
-      <Navbar />
-      <main className="flex-grow">
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/services' element={<Services/>} />
-          <Route path='/library' element={<GuidanceLibrary/>} />
-          <Route path='/islamic-guidance' element={<IslamicGuidance/>} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-    
-  )
+    <Router>
+      <div className="flex flex-col min-h-screen bg-amber-50 text-gray-800">
+        {/* HEADER with Samarkand-inspired background */}
+        <header
+          className="relative"
+          style={{
+            backgroundImage: 'url("/pattern-samarkand.png")',
+            backgroundRepeat: "repeat",
+            backgroundSize: "300px",
+            backgroundAttachment: "fixed",
+            backgroundColor: "rgba(247, 247, 245, 0.9)",
+          }}
+        >
+          <div className="bg-white/80 backdrop-blur-sm">
+            <Header />
+          </div>
+        </header>
+
+        {/* MAIN CONTENT */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
+        {/* FOOTER */}
+        <Footer />
+      </div>
+    </Router>
+  );
 }
+
+export default App;
